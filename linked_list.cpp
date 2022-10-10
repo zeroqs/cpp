34b35cd45b566dd59c2f3832595b6b1f03d50c0b
zeroqs <94167692+zeroqs@users.noreply.github.com>
2022-10-10 04:14:19 +0300

    Add files via upload

---
+++ linked_list.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <list>
+using namespace std;
+
+
+typedef struct { // Данные в списке
+    string name;
+    int avg_score;
+    bool student_type;
+}STUDENT;
+
+typedef struct link_list { // Объект списка
+    STUDENT student;
+    struct link_list* prev, * next;
+} LIST;
+
+LIST* head = NULL, * tail = NULL;
+
+void add_obj(LIST* obj, STUDENT student) {
+    LIST* ptr = new LIST;
+
+    ptr->student = student;
+    ptr->prev = obj;
+    ptr->next = (obj == NULL) ? NULL : obj->next;
+
+    if (obj != NULL) {
+        if (obj->next != NULL) obj->next->prev = ptr;
+        obj->next = ptr;
+    }
+
+    if (ptr->prev == NULL) head = ptr;
+    if (ptr->next == NULL) tail = ptr;
+}
+
+void show() {
+    LIST* c = head;
+
+    while (c != NULL) {
+        cout << c->student.name << endl;
+        c = c->next;
+    }
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+
+   
+
+
+    
+    STUDENT list = {"Алекс",5,true};
+
+    add_obj(tail, list);
+
+    show();
+
+
+    return 0; 
+}
+
