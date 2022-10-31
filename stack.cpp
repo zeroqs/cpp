853795379e70ef746bc57a7a1801ddc30d539df7
zeroqs <94167692+zeroqs@users.noreply.github.com>
2022-10-31 14:06:52 +0300

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,38 +1,105 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
+ 
+// создадим стек. как основу используем массив
 
-typedef struct {
-    int number;
-}DATA;
+// Определяем емкость stack по умолчанию
+#define SIZE 10
+ 
+// Класс для представления stack
+class Stack
+{
+    int *arr;
+    int top;
+    int capacity;
+ 
+public:
+    Stack(int size = SIZE);         // конструктор
+    ~Stack();                       // деструктор
+ 
+    void push(int);
+    int pop();
+    int peek();
 
-typedef struct obj{
-    DATA data; 
-    struct obj* next;
-}OBJ;
-
-OBJ* top = NULL;
-
-OBJ* push(DATA obj) {
-    OBJ* ptr = new OBJ; 
-
-    ptr->data = obj;
-    ptr->next = top;
-
-    top = ptr;
-    return ptr;
+};
+ 
+// Конструктор для инициализации stack
+Stack::Stack(int size)
+{
+    arr = new int[size];
+    capacity = size;
+    top = -1;
 }
-void pop() {
-    if (top == NULL) return;
-    OBJ* ptr = top->next;
-
-    cout << top->data.number << " deleted\n";
-    delete top;
-    top = ptr;
+ 
+// Деструктор для освобождения памяти, выделенной для stack
+Stack::~Stack() {
+    delete[] arr;
 }
+
+// Вспомогательная функция для добавления элемента `x` в stack
+void Stack::push(int x)
+{
+
+    cout << x;
+    arr[++top] = x;
+}
+
+// Вспомогательная функция для извлечения верхнего элемента из stack
+int Stack::pop()
+{
+
+    cout << peek();
+ 
+    // уменьшаем размер stack на 1 и (необязательно) возвращаем извлеченный элемент
+    return arr[top--];
+}
+
+
+// Вспомогательная функция для возврата верхнего элемента stack
+int Stack::peek()
+{
+    return arr[top];
+}
+
 
 int main()
 {
+    int size;
+    std::cout << "Какова размерность стека? ";
+    std::cin >> size;
+    Stack maoStack(size);
+    cout << "Стек:";
+    srand((unsigned int)time(NULL));
+    for(int i = 0;i<size;i++) {
+        maoStack.push(-20 + rand() % (80));
+        std::cout << ", ";
+    }
+    cout << "." << endl;
+    cout << "Теперь удалим из стека элементы" << endl;
+    // создадим новый стек, такой же как прошлый, но пустой
+    Stack newMaoStack(size);
+    Stack secondStack(size);
+    for(int i = 0;i<size;i++) {
+        int el = maoStack.pop();
+        std::cout << " удалено."<< endl;
+        if(el%7!=0) {
+            std::cout << "В новый стек записали ";
+            newMaoStack.push(el);
+            std::cout << "."<< endl;
+        } else {
+            secondStack.push(el);
+        }
+    }
+    cout << "Новый стек" << endl;
+    
+    for(int i = 0;i<size;i++) {
+        newMaoStack.pop(); 
+        std::cout << ", ";
+    }
+
     
 
+    
     return 0;
 }
