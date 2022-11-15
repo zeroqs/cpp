9a6ae4dfaeae50ffc6f642b3e8bad45911e32010
zeroqs <94167692+zeroqs@users.noreply.github.com>
2022-11-15 03:54:45 +0300

    my

--- stack.cpp
+++ stack.cpp
@@ -1,91 +1,58 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
+using namespace std;
 
-struct elt {
-    struct elt *next;
-    int value;
-};
-
-/* 
- * We could make a struct for this,
- * but it would have only one component,
- * so this is quicker.
- */
-typedef struct elt *Stack;
+typedef struct obj{
+    struct obj* next;
+    int number;
+}OBJ;
+typedef struct obj* Stack;
 
 #define STACK_EMPTY (0)
 
-/* push a new value onto top of stack */
-void
-stackPush(Stack *s, int value)
-{
-    struct elt *e;
 
-    e = malloc(sizeof(struct elt));
-    assert(e);
+void push(Stack *s,int number) {
+    OBJ* ptr = new OBJ;
 
-    e->value = value;
-    e->next = *s;
-    *s = e;
+    ptr->number = number;
+    ptr->next = *s;
+    *s = ptr;
 }
+//void pop() {
+//    if (top == NULL) return;
+//    OBJ* ptr = top->next;
+//
+//    cout << top->data.number << " deleted\n";
+//    delete top;
+//    top = ptr;
+//}
+void stackPrint(const Stack* s) {
+    OBJ* ptr = new OBJ;
 
-int
-stackEmpty(const Stack *s)
-{
-    return (*s == 0);
-}
 
-int
-stackPop(Stack *s)
-{
-    int ret;
-    struct elt *e;
-
-    assert(!stackEmpty(s));
-
-    ret = (*s)->value;
-
-    /* patch out first element */
-    e = *s;
-    *s = e->next;
-
-    free(e);
-
-    return ret;
-}
-
-/* print contents of stack on a single line */
-void
-stackPrint(const Stack *s)
-{
-    struct elt *e;
-
-    for(e = *s; e != 0; e = e->next) {
-        printf("%d ", e->value);
+    for (ptr = *s; ptr != 0; ptr = ptr->next) {
+        printf("%d ", ptr->number);
     }
 
     putchar('\n');
 }
+int stackEmpty(const Stack* s) {
+    return (*s == 0);
+}
 
-int
-main(int argc, char **argv)
+int main()
 {
-    int i;
-    Stack s;
-
-    s = STACK_EMPTY;
-
-    for(i = 0; i < 5; i++) {
-        printf("push %d\n", i);
-        stackPush(&s, i);
-        stackPrint(&s);
+    srand((unsigned int)time(NULL));
+    int n;
+    Stack s = STACK_EMPTY;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        int rand_number = -20 + rand() % (80);
+        push(&s, rand_number);
     }
-
-    while(!stackEmpty(&s)) {
-        printf("pop gets %d\n", stackPop(&s));
-        stackPrint(&s);
-    }
+    stackPrint(&s);
 
     return 0;
 }
