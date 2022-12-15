552878349fc4d1951ee71f1b07e4a88f64a5b8d8
zeroqs <94167692+zeroqs@users.noreply.github.com>
2022-12-15 06:27:19 +0300

    Update sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -6,27 +6,66 @@
 // Объединяем два отсортированных подмассива `arr[low…mid]` и `arr[mid+1…high]`
 void Merge(int targer_array[], int array_left[], int array_right[])
 {
-    
+    int array_left_index = 0;
+    int array_right_index = 0;
+    int index_of_insert_min_number_in_array = 0; 
+    int array_left_len = sizeof(array_left) / sizeof(array_left[0]);
+    int array_right_len = sizeof(array_right) / sizeof(array_right[0]);
+
+    while (array_left_index < array_left_len && (array_right_index < array_right_len))
+    {   
+        if (array_left[array_left_index] <= array_right[array_right_index])
+        {
+            target_array[index_of_insert_min_number_in_array] = array_left[array_left_index];
+            array_left_index++;
+        }
+        else
+        {
+            target_array[index_of_insert_min_number_in_array] = array_right[array_right_index];
+            array_right_index++;
+        }
+        index_of_insert_min_number_in_array;
+    }
+    while (array_left_index < array_left_len)
+    {
+        target_array[index_of_insert_min_number_in_array] = array_left[array_left_index];
+        array_left_index++;
+        index_of_insert_min_number_in_array;
+
+    } 
+    while (array_right_index < array_right_len)
+    {
+        target_array[index_of_insert_min_number_in_array] = array_right[array_right_index];
+        array_right_index++;
+        index_of_insert_min_number_in_array;
+
+    }
 }
 
 // Сортируем массив `arr[low…high]`, используя вспомогательный массив `aux`
 void merge_sort(int arr[], int aux[], int low, int high)
 {
-    // Базовый вариант
-    if (high == low) {        // если размер прогона == 1
+    int array_len = sizeof(array) / sizeof(array[0]);
+
+    if (array_len < 2) {
         return;
     }
+    
+    int mid = array_len / 2;
+    int* left = new int[mid];
+    int* right = new int[array_len - mid];
 
-    // найти середину
-    int mid = (low + ((high - low) >> 1));
-
-    // рекурсивное разделение выполняется на две половины до тех пор, пока размер выполнения не станет == 1,
-    // затем объединяем их и возвращаемся вверх по цепочке вызовов
-
-    merge_sort(arr, aux, low, mid);          // разделить/объединить левую половину
-    merge_sort(arr, aux, mid + 1, high);     // разделить/объединить правую половину
-
-    Merge(arr, aux, low, mid, high);        // объединить два полупрогона.
+    for (int i = 0; i < mid; i++)
+    {
+        left[i] = array[i];
+    }
+    for (int i = mid; i < array_len; i++)
+    {
+        right[i] = array[i];
+    }
+    MergeSort(left);
+    MergeSort(right);
+    Merge(array, left, right);     // объединить два полупрогона.
 }
 
 
